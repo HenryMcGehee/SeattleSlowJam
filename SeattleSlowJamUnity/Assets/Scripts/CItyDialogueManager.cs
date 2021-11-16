a4dbf21d6005c3c49521a6a380a715fe92badc33
hdmcgehee <hdmcgehee@gmail.com>
2021-11-15 19:06:24 -0800

    city level block out

---
+++ SeattleSlowJamUnity/Assets/Scripts/CItyDialogueManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+public class CItyDialogueManager : MonoBehaviour
+{
+    public Flowchart chart;
+    public FadeManager fade;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fade.SetFade(1,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayDialogue(int i){
+        chart.ExecuteBlock(i.ToString());
+    }
+}
---
+++ SeattleSlowJamUnity/Assets/Scripts/PlaceObjectFromMouse.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceObjectFromMouse : MonoBehaviour
+{
+    public GameObject[] prefabs;
+
+    private GameObject currentObject;
+
+    public GameObject gameManager;
+    private int objIndex;
+    public int diaIndex;
+
+    void Start(){
+        gameManager = GameObject.FindGameObjectWithTag("GameManager");
+
+        if(currentObject == null){
+            currentObject = Instantiate(prefabs[0]);
+        }
+        else{
+            Destroy(currentObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveObject();
+    }
+
+    private void MoveObject(){
+        if(currentObject != null){
+
+            Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+            RaycastHit hit;
+            int layerMask =~ LayerMask.GetMask("CurrentObject");
+
+            if (Physics.Raycast (ray, out hit, 40000, layerMask)) 
+            {
+                currentObject.transform.position = hit.point;        
+                currentObject.transform.rotation = Quaternion.FromToRotation(Vector3.up, hit.normal);        
+            }    
+            
+            if(Input.GetMouseButtonDown(0)){
+                Instantiate(currentObject, hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal));
+                //NextObject();
+                currentObject = null;
+                gameManager.GetComponent<CItyDialogueManager>().PlayDialogue(diaIndex);
+                diaIndex++;
+            }
+        }
+    }
+
+    private void NextObject(){
+        if(diaIndex > 2){
+            Debug.Log("fffff");
+            gameManager.GetComponent<CameraController>().cameraSwitcher(0);
+        }
+
+        if(objIndex > prefabs.Length){
+            objIndex = 0;
+            currentObject = Instantiate(prefabs[objIndex]);
+        }
+        else{
+            objIndex++;
+            currentObject = Instantiate(prefabs[objIndex]);
+        }
+    }
+}
